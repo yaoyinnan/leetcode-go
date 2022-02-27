6c7a9d4f370d1b6d0762f0f157014085d65ae304
yaoyinnan@foxmail.com <yaoyinnan@foxmail.com>
2022-02-27 10:49:19 +0800

    010-fib

---
+++ jzoffer1/010-fib/main.go
@@ -0,0 +1,61 @@
+// Copyright 2022 <yaoyinnan@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fib0(5))
+	fmt.Println(fib0(100))
+
+	fmt.Println(fib1(5))
+	fmt.Println(fib1(100))
+}
+
+func fib0(n int) int {
+	const mod int = 1e9 + 7
+	if n < 2 {
+		return n
+	}
+	p, q, r := 0, 0, 1
+	for i := 2; i <= n; i++ {
+		p = q
+		q = r
+		r = (p + q) % mod
+	}
+	return r
+}
+
+const mod int = 1e9 + 7
+
+type matrix [2][2]int
+
+func multiply(a, b matrix) (c matrix) {
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			c[i][j] = (a[i][0]*b[0][j] + a[i][1]*b[1][j]) % mod
+		}
+	}
+	return
+}
+
+func pow(a matrix, n int) matrix {
+	ret := matrix{{1, 0}, {0, 1}}
+	for ; n > 0; n >>= 1 {
+		if n&1 == 1 {
+			ret = multiply(ret, a)
+		}
+		a = multiply(a, a)
+	}
+	return ret
+}
+
+func fib1(n int) int {
+	if n < 2 {
+		return n
+	}
+	res := pow(matrix{{1, 1}, {1, 0}}, n-1)
+	return res[0][0]
+}
